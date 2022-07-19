8210e25983038174fb2833d26e48bd981a90ce26
ozovalihasan <ozovalihasan@gmail.com>
2022-07-19 20:07:59 +0300

    Update the used formula to calculate the quotation price

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def calculate_quotation(municipal_evaluation)
     
-    prime_avant_TX = (((municipal_evaluation.to_f-500000)/1000)*1.17)+320
-    emission_fees = 20
-    tax = prime_avant_TX * 9 / 100
+    prime_avant_TX = (((municipal_evaluation.to_f-500000.0 ) / 1000.0 )* 1.17 ) + 320.0
+    emission_fees = 20.0
+    tax = prime_avant_TX * 9.0 / 100.0
     prime_avant_TX + emission_fees + tax
   end
 end
